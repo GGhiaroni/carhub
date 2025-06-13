7db467d1b5c4ef818c8ca6856213d820d073ecc6
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-13 15:16:11 -0300

    criando os componentes Navbar e Footer e importando no layout

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,7 +25,13 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="" />
+          <Image
+            src="/hero.png"
+            alt="imagem carro hero"
+            fill
+            className="object-contain"
+          />
+          <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>
--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
 import BotaoEstilizado from "./BotaoEstilizado";
+import Footer from "./Footer";
 import Hero from "./Hero";
+import Navbar from "./Navbar";
 
-export { BotaoEstilizado, Hero };
+export { BotaoEstilizado, Footer, Hero, Navbar };
