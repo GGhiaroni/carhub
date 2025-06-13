6dd05a3b25ed0215ed3b8560abe21a5332209571
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-12 21:21:28 -0300

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 text-red-500 pt-36 padding-x">
+        <h1 className="hero__title">
+          Encontre, reserve ou alugue um carro de forma rápida e fácil.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
