3f47b32ecf305d8c5167d06ccc9c639dcaf83841
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-15 21:13:05 -0300

    comecando a criar a section de catalogo de carros

--- app/page.js
+++ app/page.js
@@ -4,6 +4,16 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="descubra">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Catálogo de modelos</h1>
+          <p>Explore os carros que você pode gostar</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container"></div>
+        </div>
+      </div>
     </main>
   );
 }
