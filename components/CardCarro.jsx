fc5b0b2c11f930c75298fc5901e81d0760ff4c49
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-16 13:27:18 -0300

    avancando com o design dos cards dos carros

--- components/CardCarro.jsx
+++ components/CardCarro.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { calcularValorAluguelCarro } from "@/utils";
+import Image from "next/image";
 
 const CardCarro = ({ carro }) => {
   const { year, make, model, transmission, drive } = carro;
@@ -19,6 +20,28 @@ const CardCarro = ({ carro }) => {
         {custoAluguel}
         <span className="self-end text-[14px] font-medium">/dia</span>
       </p>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="modelo do carro"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2 ">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="ícone volante"
+            />
+            <p className="text-[14px]"></p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
