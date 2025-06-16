5dc2bdcffbef05d9e64b7c6cd88ef04335fe88a5
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-16 13:07:25 -0300

    comecando a desenvolver o componente CardCarro

--- components/CardCarro.jsx
+++ components/CardCarro.jsx
@@ -1,5 +1,19 @@
-const CardCarro = () => {
-  return <div>CardCarro</div>;
+"use client";
+
+const CardCarro = ({ carro }) => {
+  const { city_mpg, year, make, model, transmission, drive } = carro;
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p>
+        <span>custo</span>
+      </p>
+    </div>
+  );
 };
 
 export default CardCarro;
