5233bdcd5df510f051a2d2841598206ccea962a6
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-18 17:31:16 -0300

    criando em utils a funcao responsavel por gerar a imagem do carro

--- utils/index.js
+++ utils/index.js
@@ -81,3 +81,18 @@ export const transformarPrimeiraLetraMaiuscula = (texto) => {
     )
     .join(" ");
 };
+
+export const gerarImagemCarroUrl = (carro, angle) => {
+  const url = new URL("https://cdn.imagin/studio/getimage");
+
+  const { make, year, model } = carro;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
