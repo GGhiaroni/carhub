0ec5e42b502781615a6204e3b5021df7d5336bad
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-19 13:02:05 -0300

    corrigindo bug para mostrar o icone de lupa na barra de pesquisa

--- components/BarraDePesquisa.jsx
+++ components/BarraDePesquisa.jsx
@@ -6,9 +6,10 @@ import { BuscarFabricantes } from ".";
 
 const BarraDePesquisa = () => {
   const [fabricante, setFabricante] = useState("");
+  const [modelo, setModelo] = useState("");
 
-  const SearchButton = ({ outrasClasses }) => {
-    <button type="submit" className={`-ml-3 z-10 ${outrasClasses}`}>
+  const SearchButton = ({ outrasClasses }) => (
+    <button type="submit" className={`-ml-3 z-100 ${outrasClasses}`}>
       <Image
         src="/magnifying-glass.svg"
         alt="ícone lupa"
@@ -16,8 +17,8 @@ const BarraDePesquisa = () => {
         height={40}
         className="object-contain"
       />
-    </button>;
-  };
+    </button>
+  );
 
   const handleSearch = () => {};
 
@@ -36,8 +37,19 @@ const BarraDePesquisa = () => {
           width={25}
           height={25}
           className="absolute w-[20px] h-[20px] ml-4"
+          alt="ícone modelo do carro"
         />
+        <input
+          type="text"
+          name="modelo"
+          value={modelo}
+          onChange={(e) => setModelo(e.target.value)}
+          className="searchbar__input"
+          placeholder="Modelo"
+        />
+        <SearchButton outrasClasses="sm:hidden" />
       </div>
+      <SearchButton outrasClasses="max-sm:hidden" />
     </form>
   );
 };
