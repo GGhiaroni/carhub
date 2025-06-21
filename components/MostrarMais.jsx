6bec4a957a7bfbb61071ba6f30c16ce7e99fc22a
GGhiaroni <gghiaronitiziano@gmail.com>
2025-06-21 17:00:00 -0300

    criando a lógica de mostrar mais c arros a partir do clique no botão MostrarMais, por mais que o plano free da API forneca apenas 1 carro por vez

--- components/MostrarMais.jsx
+++ components/MostrarMais.jsx
@@ -1,7 +1,31 @@
 "use client";
 
+import { atualizarSearchParams } from "@/utils";
+import { useRouter } from "next/navigation";
+import { BotaoEstilizado } from ".";
+
 const MostrarMais = ({ numeroPagina, isNext }) => {
-  return <div>MostrarMais</div>;
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const novoLimite = (numeroPagina + 1) * 10;
+    const novoCaminho = atualizarSearchParams("limit", `${novoLimite}`);
+    router.push(novoCaminho, { scroll: false });
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {isNext && (
+        <BotaoEstilizado
+          titulo="Mostrar mais"
+          btnType="button"
+          estiloContainer="bg-primary-blue rounded-full
+                text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default MostrarMais;
